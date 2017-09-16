1b68e9460595e7e407a55a7bbf1cc1a14ec08b9a
Marcos Barbosa <barbosa.mg@gmail.com>
2017-09-16 01:29:26 -0300

    Criando diretiva

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 require('angular');
 require('./locale/angular-locale_pt-br');
 
-angular.module('app', []);
 
 var Maincontroller = require('./controllers/MainController');
+var maskTel = require('./directives/maskTel');
 
 
+angular.module('app', []);
+angular.module('app').directive('maskTel', [maskTel]);
 angular.module('app').controller('MainController', ['$scope', '$http', '$filter', Maincontroller]);
---
+++ app/directives/maskTel.js
@@ -0,0 +1,39 @@
+module.exports = function() {
+
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attributes, ctrl) {
+            // console.log(scope);
+            // console.log(element);
+            // console.log(attributes);
+
+            element.bind('keyup', function() {
+                // console.log(scope.formClient.telClient.$viewValue);
+                // console.log(ctrl.$viewValue);
+
+                var formatTel = function (value) {
+                    value = value.replace(/[^0-9]/g, '').substring(0, 9);
+
+                    if(value.length > 4 && value.length <= 8) {
+                        value = value.substring(0, 4) + '-' + value.substring(4, 8);
+                    } else if(value.length > 4 && value.length <= 9) {
+                        value = value.substring(0, 5) + '-' + value.substring(5, 9);
+                    }
+
+                    return value;
+
+                };
+
+                ctrl.$setViewValue(formatTel(ctrl.$viewValue));
+                ctrl.$render();
+
+            });
+
+            ctrl.$parsers.push(function(value) {
+                value = value.replace(/[^0-9]/g, '');
+                return value;
+            });
+        }
+    };
+
+}
